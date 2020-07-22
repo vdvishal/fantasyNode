97785fa7622ff6d7bc5666a94df26ae553b05c30
Vishal Dutta <36704086+vdvishal@users.noreply.github.com>
2020-07-22 19:26:24 +0530

    feat: PG

---
+++ app/controllers/pg/transaction.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Orders = mongoose.model('Orders');
+const User = mongoose.model('Users');
+
+
+
+const getOrder = (req,res) => {
+    Orders.find({"notes.userId":req.user.id,status:"paid"})
+                                .sort({_id:-1})
+                                .skip(req.query.page*50)
+                                .limit(50)
+                                .exec()
+                                .then(response =>{res.status(200).json(response)})
+}
+
+module.exports = getOrder;
--- app/routes/pg.js
+++ app/routes/pg.js
@@ -1,5 +1,6 @@
 const pg = require('../controllers/pg/index')
 const success = require('../controllers/pg/success')
+const transaction = require('../controllers/pg/transaction')
 
 const isAuth = require('../middlewares/passport/isAuthenticate')
 
@@ -8,6 +9,8 @@ module.exports.setRouter = (app) => {
     app.post(`${process.env.BASE_URL}/payment/`,isAuth,pg);
 
     app.post(`${process.env.BASE_URL}/payment/success`,isAuth,success);
+
+    app.get(`${process.env.BASE_URL}/payment/transaction`,isAuth,transaction);
 
 }
 
