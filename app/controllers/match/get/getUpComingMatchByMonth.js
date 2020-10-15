37fb4d5dbc8bb45d27b4f0905850cfa4aa0c0cb2
Vishal Dutta <36704086+vdvishal@users.noreply.github.com>
2020-10-16 01:17:08 +0530

    #

--- app/controllers/match/get/getUpComingMatchByMonth.js
+++ app/controllers/match/get/getUpComingMatchByMonth.js
@@ -7,16 +7,51 @@ const instance = axios.create({
 
 const getUpComing = (req, res) => {
    //2020-01-01,2020-11-01
-   console.log(req.params);
-   
-    instance.get(
-        `/fixtures?api_token=${process.env.Access_key}&include=league,season,visitorteam,localteam&sort=starting_at&filter[starts_between]=${req.params.start},${req.params.end}`)
-    .then(response => {                
-        res.status(200).json(response.data)
-    }).catch(err => {
-        console.log(err);
+   console.log('req.params: ', req.params);
+   console.log('req.query: ', req.query);
+   switch (req.query.gameType) {
+       case 1:
+        instance.get(
+            `/fixtures?api_token=${process.env.Access_key}&include=league,season,visitorteam,localteam&sort=starting_at&filter[starts_between]=${req.params.start},${req.params.end}`)
+        .then(response => {                
+            res.status(200).json(response.data)
+        }).catch(err => {
+            console.log(err);
+            
+        })
+           break;
         
-    })
+        case 2:
+            instance.get(
+                `/fixtures?api_token=${process.env.Access_key}&include=league,season,visitorteam,localteam&sort=starting_at&filter[starts_between]=${req.params.start},${req.params.end}`)
+            .then(response => {                
+                res.status(200).json(response.data)
+            }).catch(err => {
+                console.log(err);
+                
+            })
+               break;
+   
+       default:
+        instance.get(
+            `/fixtures?api_token=${process.env.Access_key}&include=league,season,visitorteam,localteam&sort=starting_at&filter[starts_between]=${req.params.start},${req.params.end}`)
+        .then(response => {                
+            res.status(200).json(response.data)
+        }).catch(err => {
+            console.log(err);
+            
+        })
+           break;
+   }
+   
+    // instance.get(
+    //     `/fixtures?api_token=${process.env.Access_key}&include=league,season,visitorteam,localteam&sort=starting_at&filter[starts_between]=${req.params.start},${req.params.end}`)
+    // .then(response => {                
+    //     res.status(200).json(response.data)
+    // }).catch(err => {
+    //     console.log(err);
+        
+    // })
   
 }
 
