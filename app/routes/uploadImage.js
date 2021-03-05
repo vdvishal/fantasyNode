799988bd37945d7ee9272d868f08562d2fbabdaa
Vishal Dutta <36704086+vdvishal@users.noreply.github.com>
2021-03-06 01:12:31 +0530

    #

--- app/routes/uploadImage.js
+++ app/routes/uploadImage.js
@@ -6,22 +6,24 @@ const isAuth = require('../middlewares/jwt/auth')
  
 const image = require('../controllers/uploadImage/uploadImage')
 
-var multer  = require('multer')
-var storage = multer.diskStorage(
-    {
-        destination: '/var/www/html/images/',
-        filename: function ( req, file, cb ) {
-            cb( null, Date.now() + '.' + file.mimetype.split('/')[1]);
-        }
-    }
-);
 
-let upload = multer({ storage: storage });
+/*  UNCOMMENT AFTER USE */
+// var multer  = require('multer')
+// var storage = multer.diskStorage(
+//     {
+//         destination: '/var/www/html/images/',
+//         filename: function ( req, file, cb ) {
+//             cb( null, Date.now() + '.' + file.mimetype.split('/')[1]);
+//         }
+//     }
+// );
+
+// let upload = multer({ storage: storage });
 let baseUrl = `${process.env.BASE_URL}/image`;
 
 
 module.exports.setRouter = (app) => {
-    app.post(`${baseUrl}/upload`,upload.single('image'),isAuth,image)
+    // app.post(`${baseUrl}/upload`,upload.single('image'),isAuth,image)
     
     
     app.post(`${baseUrl}/admin/upload`,upload.single('image'),image)
